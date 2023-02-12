b7c0ce186a540d2ed9f2243b272449c92d1ae0e8
Srijit Bera <berasrijit02@gmail.com>
2023-02-12 18:34:19 +0530

    Update Prime_List.cpp

--- Prime_List.cpp
+++ Prime_List.cpp
@@ -1,3 +1,18 @@
+
+// You are given the head of a linked list. You have to replace all the values of the nodes with the nearest prime number. If more than one prime number exists at an equal distance, choose the smallest one.
+
+// Example 1:
+
+// Input:
+// 2 → 6 → 10
+// Output:
+// 2 → 5 → 11
+// Explanation:
+// The nearest prime of 2 is 2 itself.
+// The nearest primes of 6 are 5 and 7,
+// since 5 is smaller so, 5 will be chosen.
+// The nearest prime of 10 is 11.
+
 class Solution{
 public:
     Node *primeList(Node *head){
